cd8adac3d2dd113dd9d799467b8939b67ad4fd6b
rinjugatla <rin.jugatla@gmail.com>
2023-03-07 11:55:27 +0900

    GUIMapManager Showメソッドにフックできない不具合を修正

--- HoboToughLifeMods/HoboUtility.cs
+++ HoboToughLifeMods/HoboUtility.cs
@@ -3,6 +3,7 @@ using HarmonyLib;
 using UnityEngine;
 using Game;
 using UI;
+using System;
 
 namespace HoboToughLifeMods
 {
@@ -19,19 +20,35 @@ namespace HoboToughLifeMods
             GUI.Box(new Rect(0, 0, 300, 500), "My Menu");
         }
 
-        [HarmonyPatch(typeof(GUIMapManager), "Show")]
+        [HarmonyPatch(typeof(GUIMapManager), "Show", new Type[] { typeof(bool) })]
         static class GUIMapManager_Hook
         {
-            static void Postfix(GUIMapManager __instance, bool __0)
+            static void Postfix(GUIMapManager __instance, bool hasMap)
             {
                 var guiMapObject = GameObject.Find("GUI/GUICanvas/CanvasScaler/GUIMap/transl");
-                Debug.Log(guiMapObject.transform);
-                foreach (Transform trans in guiMapObject.transform)
+                Debug.Log($"A: {guiMapObject.transform}");
+                
+                // Good
+                foreach (var trans in guiMapObject.transform)
                 {
-                    Debug.Log(trans.gameObject.name);
+                    var casted = trans.TryCast<Transform>();
+                    Debug.Log($"B: {casted.gameObject.name}");
                 }
 
-                var objects = UnityEngine.GameObject.FindObjectsOfType<UnityEngine.GameObject>();
+                // Bad
+                //[ERROR] Exception in Harmony patch of method void UI.GUIMapManager::Show(bool hasMap):
+                //System.InvalidCastException: Specified cast is not valid.
+                //  at HoboToughLifeMods.HoboUtility + GUIMapManager_Hook.Postfix(UI.GUIMapManager __instance, System.Boolean __0)[0x0002d] in < 2cecffa515584304a7faeb001594e823 >:0
+                //  at(wrapper dynamic - method) UI.GUIMapManager.DMD<UI.GUIMapManager::Show>(UI.GUIMapManager, bool)
+                //  at(wrapper dynamic - method) MonoMod.Utils.DynamicMethodDefinition.DMD<UI.GUIMapManager::Show> _il2cpp(intptr, bool)
+                //foreach (Transform trans in guiMapObject.transform)
+                //{
+                //    var casted = trans.TryCast<Transform>();
+                //    Debug.Log($"B: {casted.gameObject.name}");
+                //}
+
+
+                var objects = GameObject.FindObjectsOfType<GameObject>();
                 foreach (var @object in objects)
                 {
                     bool isRootObject = @object.transform.parent == null;
@@ -39,13 +56,13 @@ namespace HoboToughLifeMods
                     {
                         continue;
                     }
-                    bool isPlayer = @object.GetComponent<Game.PlayerManager>() != null;
+                    bool isPlayer = @object.GetComponent<PlayerManager>() != null;
                     if (!isPlayer)
                     {
                         continue;
                     }
 
-                    Debug.Log(@object.name);
+                    Debug.Log($"C: {@object.name}");
                 }
             }
         }
