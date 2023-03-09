8955800c9709d47ead27d5861e8a62170e534388
rinjugatla <rin.jugatla@gmail.com>
2023-03-09 16:39:10 +0900

    マップを開いた際にップに表示する機能を実装
    ゲーム上は元からあるアイコンとかぶって表示されるため存在はわからない(UnityExprolerなどを使用するしか視覚的に確認できない)

--- HoboToughLifeMods/HoboUtility.cs
+++ HoboToughLifeMods/HoboUtility.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using Game;
 using UI;
 using System;
+using Core.Net;
+using Game.ArcadeMachine;
+using Core;
 
 namespace HoboToughLifeMods
 {
@@ -12,58 +15,90 @@ namespace HoboToughLifeMods
         public override void OnInitializeMelon()
         {
             LoggerInstance.Msg("Hello HoboUtility");
-            MelonEvents.OnGUI.Subscribe(DrawMenu, 100);
-        }
-
-        private void DrawMenu()
-        {
-            GUI.Box(new Rect(0, 0, 300, 500), "My Menu");
         }
 
         [HarmonyPatch(typeof(GUIMapManager), "Show", new Type[] { typeof(bool) })]
         static class GUIMapManager_Hook
         {
+            private static UniversalNetworkPlayer MyNetworkPlayer = null;
+            private static GameObject DammyMapPlayer = default;
+            private static GUIMapPlayerPoint DammyMapPlayerPoint = default;
+            private static GameObject CanvasObject = default;
+
             static void Postfix(GUIMapManager __instance, bool hasMap)
             {
-                var guiMapObject = GameObject.Find("GUI/GUICanvas/CanvasScaler/GUIMap/transl");
-                Debug.Log($"A: {guiMapObject.transform}");
+                SetMyNetworkPlayer();
+                CreateDammyMapPlayerObject();
+                UpdateDammyMapPlayer();
+            }
+
+            /// <summary>
+            /// 自キャラのコンポーネントを取得
+            /// </summary>
+            static void SetMyNetworkPlayer()
+            {
+                if (MyNetworkPlayer != null) { return; }
+
+                var playerObject = GameObject.FindObjectOfType<UniversalNetworkPlayer>();
+                if(playerObject == null) { return; }
+
+                MyNetworkPlayer = playerObject.GetComponent<UniversalNetworkPlayer>();
+            }
+
+            /// <summary>
+            /// マップにダミーアイコンを作成
+            /// </summary>
+            static void CreateDammyMapPlayerObject()
+            {
+                if (MyNetworkPlayer == null) { return; }
+
+                bool hasCreated = DammyMapPlayer != null;
+                if (hasCreated) { return; }
+
+                var mapPlayer = GameObject.Find("GUI/GUICanvas/CanvasScaler/GUIMap/transl/MapPlayer");
+                var dammyMapPlayer  = CloneObject(mapPlayer, "DammyMapPlayer");
+                if(dammyMapPlayer == null)
+                {
+                    Debug.Log("クローンに失敗");
+                    return;
+                }
+
+                var playerPoint = dammyMapPlayer.GetComponent<GUIMapPlayerPoint>();
+                playerPoint.myPlayer = MyNetworkPlayer;
+                DammyMapPlayer = dammyMapPlayer;
+                DammyMapPlayerPoint = playerPoint;
+            }
+
+            /// <summary>
+            /// GameObjectをコピー
+            /// </summary>
+            /// <param name="origin">元オブジェクト</param>
+            /// <param name="copyName">コピーオブジェクト名</param>
+            static GameObject CloneObject(GameObject origin, string copyName)
+            {
+                if(origin == null || copyName == null || copyName == "") { return null; }
+
+                var copy = GameObject.Instantiate(origin);
+                var originTrans = origin.transform;
+                var copyTrans = copy.transform;
                 
-                // Good
-                foreach (var trans in guiMapObject.transform)
-                {
-                    var casted = trans.TryCast<Transform>();
-                    Debug.Log($"B: {casted.gameObject.name}");
-                }
+                copy.name = copyName;
+                copyTrans.SetParent(originTrans.parent);
+                copyTrans.localPosition = originTrans.localPosition;
+                copyTrans.localScale = originTrans.localScale;
 
-                // Bad
-                //[ERROR] Exception in Harmony patch of method void UI.GUIMapManager::Show(bool hasMap):
-                //System.InvalidCastException: Specified cast is not valid.
-                //  at HoboToughLifeMods.HoboUtility + GUIMapManager_Hook.Postfix(UI.GUIMapManager __instance, System.Boolean __0)[0x0002d] in < 2cecffa515584304a7faeb001594e823 >:0
-                //  at(wrapper dynamic - method) UI.GUIMapManager.DMD<UI.GUIMapManager::Show>(UI.GUIMapManager, bool)
-                //  at(wrapper dynamic - method) MonoMod.Utils.DynamicMethodDefinition.DMD<UI.GUIMapManager::Show> _il2cpp(intptr, bool)
-                //foreach (Transform trans in guiMapObject.transform)
-                //{
-                //    var casted = trans.TryCast<Transform>();
-                //    Debug.Log($"B: {casted.gameObject.name}");
-                //}
+                return copy;
+            }
 
+            /// <summary>
+            /// アイコンを表示
+            /// </summary>
+            /// <remarks>これを実行すると紐づくユーザ情報にしたがってアイコンの位置が自動調整される</remarks>
+            static void UpdateDammyMapPlayer()
+            {
+                if (DammyMapPlayerPoint == null) { return; }
 
-                var objects = GameObject.FindObjectsOfType<GameObject>();
-                foreach (var @object in objects)
-                {
-                    bool isRootObject = @object.transform.parent == null;
-                    if (!isRootObject)
-                    {
-                        continue;
-                    }
-                    bool isPlayer = @object.GetComponent<PlayerManager>() != null;
-                    if (!isPlayer)
-                    {
-                        continue;
-                    }
-
-                    Debug.Log($"C: {@object.name}");
-                }
+                DammyMapPlayerPoint.OnShow();
             }
         }
     }
