63eb5eb5e22435ce7e023bb69e93b6489398a137
rinjugatla <rin.jugatla@gmail.com>
2023-03-17 00:11:11 +0900

    マップに表示するマルチプレイヤーのアイコンをリアルタイムで更新する機能に変更
    IL2CPPでBehaviourオブジェクトをアタッチする場合は
    クラスに[RegisterTypeInIl2Cpp]属性を付け
    ポインタを持つコンストラクタを定義すること
    public MapPlayerBehaviour(IntPtr ptr) : base(ptr) { }

---
+++ HoboToughLifeMods/Behaviour/MapPlayerBehaviour.cs
@@ -0,0 +1,97 @@
+﻿using Core.Net;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UI;
+using UnityEngine;
+
+namespace HoboToughLifeMods.Behaviour
+{
+    [RegisterTypeInIl2Cpp]
+    internal class MapPlayerBehaviour : MonoBehaviour
+    {
+        /// <summary>
+        /// 初期化が完了したか
+        /// </summary>
+        private bool IsInitEnd = false;
+        /// <summary>
+        /// プレイヤー情報
+        /// </summary>
+        private GameObject PlayerObject { get; set; }
+        /// <summary>
+        /// プレイヤーがゲーム内に存在するか
+        /// </summary>
+        public bool Exists => PlayerObject != null;
+        /// <summary>
+        /// プレイヤー名
+        /// </summary>
+        public string Name => Exists ? PlayerObject.name : "";
+
+        public MapPlayerBehaviour(IntPtr ptr) : base(ptr) { }
+
+        /// <summary>
+        /// 初期化(アイコン表示に必要な情報を作成)
+        /// </summary>
+        /// <param name="playerObject">プレイヤー情報</param>
+        public void Init(GameObject playerObject)
+        {
+            PlayerObject = playerObject;
+            IsInitEnd = true;
+        }
+
+        private void Update()
+        {
+            if (IsInitEnd && !Exists)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            UpdateMapPlayer();
+        }
+
+        /// <summary>
+        /// アイコン位置を更新して表示
+        /// </summary>
+        /// <remarks>紐づくユーザ情報に従ってアイコンの位置や方向が自動調整される</remarks>
+        private void UpdateMapPlayer()
+        {
+            if (!Exists) { return; }
+
+            var gameTrans = PlayerObject.transform;
+            var mapTrans = gameObject.transform;
+            mapTrans.position = ConvertGameToMapPostion(gameTrans.position);
+            mapTrans.eulerAngles = ConvertGameToMapAngle(gameTrans.eulerAngles);
+        }
+
+        /// <summary>
+        /// ゲーム位置からマップ位置に変換
+        /// </summary>
+        /// <param name="current">ゲーム上の位置</param>
+        private Vector3 ConvertGameToMapPostion(Vector3 current)
+        {
+            // GameObjectのPositionから最小二乗法で近似式算出
+            // 入力のx, y, zと出力のx, y, zが異なるので注意
+            float x = 0.00000630598925f * (current.z * current.z) + -0.976941344f * current.z + 874.85253f;
+            float y = -0.000000102722494f * (current.x * current.x) + 0.955787392f * current.x + 1041.58575f;
+            float z = -69.75f;
+            Vector3 result = new Vector3(x, y, z);
+            return result;
+        }
+
+        /// <summary>
+        /// ゲーム角度からマップ角度に変換
+        /// </summary>
+        /// <param name="angle">ゲーム上の角度</param>
+        private Vector3 ConvertGameToMapAngle(Vector3 angle)
+        {
+            float converted = (540f - angle.y) % 360f;
+            Vector3 result = new Vector3(0, 0, converted);
+            return result;
+        }
+    }
+}
--- HoboToughLifeMods/Controller/MapPlayerController.cs
+++ HoboToughLifeMods/Controller/MapPlayerController.cs
@@ -1,14 +1,16 @@
-﻿using HoboToughLifeMods.Model;
+﻿using HoboToughLifeMods.Behaviour;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
+using TMPro;
 using UnityEngine;
 
 namespace HoboToughLifeMods.Controller
 {
     internal class MapPlayerController 
     {
-        private List<MapPlayerModel> MapPlayers = new List<MapPlayerModel>();
+        private List<MapPlayerBehaviour> MapPlayers = new List<MapPlayerBehaviour>();
 
         /// <summary>
         /// アイコンを追加
@@ -22,22 +24,86 @@ namespace HoboToughLifeMods.Controller
             bool hasPlayerObject = MapPlayers.Select(p => p.Name).Contains(playerObject.name);
             if (hasPlayerObject) { return false; }
 
-            var model = new MapPlayerModel(playerObject);
+            var model = CreateMapPlayerObject(playerObject);
             if(model != null) { MapPlayers.Add(model); }
 
             return true;
         }
 
         /// <summary>
+        /// プレイヤーアイコンを管理するGameObjectの生成
+        /// </summary>
+        /// <param name="playerObject">紐づけるプレイヤー情報</param>
+        private MapPlayerBehaviour CreateMapPlayerObject(GameObject playerObject)
+        {
+            // 既存のアイコンを流用
+            var originMapObject = GameObject.Find("GUI/GUICanvas/CanvasScaler/GUIMap/transl/MapPlayer");
+            var copiedMapObject = CloneObject(originMapObject, playerObject.name);
+            if (originMapObject == null)
+            {
+                Debug.LogError("マップアイコンの作成に失敗");
+                return null;
+            }
+
+            AddNameObject(copiedMapObject, playerObject.name);
+            
+            var controller  = copiedMapObject.AddComponent<MapPlayerBehaviour>();
+            controller.Init(playerObject);
+
+            return controller;
+        }
+
+        /// <summary>
+        /// GameObjectをコピー
+        /// </summary>
+        /// <param name="origin">元オブジェクト</param>
+        /// <param name="copyName">コピーオブジェクト名</param>
+        private GameObject CloneObject(GameObject origin, string copyName)
+        {
+            if (origin == null || copyName == null || copyName == "") { return null; }
+
+            var copy = GameObject.Instantiate(origin);
+            var originTrans = origin.transform;
+            var copyTrans = copy.transform;
+
+            copy.name = copyName;
+            copyTrans.SetParent(originTrans.parent, false);
+            copyTrans.localPosition = Vector3.zero;
+            copyTrans.localScale = originTrans.localScale;
+
+            return copy;
+        }
+
+        /// <summary>
+        /// 名前をテキスト表示
+        /// </summary>
+        /// <remarks>ImageコンポーネントとTextコンポーネントは共存できないのでサブオブジェクトを追加してTextを表示する</remarks>
+        /// <param name="object"></param>
+        private void AddNameObject(GameObject parent, string name)
+        {
+            var textObject = new GameObject();
+            textObject.name = "PlayerNameText";
+            var trans = textObject.transform;
+            trans.SetParent(parent.transform, false);
+
+            var text = textObject.AddComponent<TextMeshProUGUI>();
+            text.text = name;
+
+            text.m_width = 100;
+            text.alignment = TextAlignmentOptions.Top;
+            text.fontSize = 20;
+            text.fontStyle = FontStyles.Bold;
+            text.color = Color.black;
+            text.enableWordWrapping = false;
+        }
+
+
+
+        /// <summary>
         /// アイコン位置を更新して表示
         /// </summary>
         public void UpdateMapPlayer()
         {
-            foreach (var player in MapPlayers) {
-                if (player.Exists) { player.UpdateMapPlayer(); }
-                else { player.Dispose(); }
-            }
-
             MapPlayers.RemoveAll(p => !p.Exists);
         }
     }
